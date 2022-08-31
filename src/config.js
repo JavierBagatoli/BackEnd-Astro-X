3dd3ca3ea897b7187ed4a229756d15c4af673a9e
JavierBagatoli <javi.bagatoli@gmail.com>
2022-08-31 10:07:12 -0300

    Chore: hardcodei la uri

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import { config } from "dotenv"
 config()
 
 export default{
-    mongodbURL: process.env.MONGODB_URI,
+    mongodbURL: process.env.MONGODB_URI || "mongodb+srv://AstroX:AstroX@astro-x.jpi1jjd.mongodb.net/?retryWrites=true&w=majority",
 }
